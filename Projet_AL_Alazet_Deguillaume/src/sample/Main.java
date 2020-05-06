7b7cb423aaec7dd1ec90a30ce0b30bfbdef33f15
Nicolas Deguillaume <nicolas.deguillaume@etu.u-bordeaux.fr>
2020-05-06 19:49:47 +0200

    la view est trop fraiche

--- Projet_AL_Alazet_Deguillaume/src/sample/Main.java
+++ Projet_AL_Alazet_Deguillaume/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        primaryStage.setTitle("BeWizU");
+        primaryStage.setScene(new Scene(root, 1080, 650));
         primaryStage.show();
     }
 
