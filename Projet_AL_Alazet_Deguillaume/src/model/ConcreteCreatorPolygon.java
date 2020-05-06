915c447bf621ce45cc2a814cdd48ce9a16a278b7
Nicolas Deguillaume <nicolas.deguillaume@etu.u-bordeaux.fr>
2020-05-06 17:55:22 +0200

    factory pattern

--- Projet_AL_Alazet_Deguillaume/src/model/ConcreteCreatorPolygon.java
+++
@@ -1,2 +0,0 @@
-public class ConcreteCreatorPolygon extends ShapeFactory {
-}
--- Projet_AL_Alazet_Deguillaume/src/model/ShapeFactory.java
+++ Projet_AL_Alazet_Deguillaume/src/model/ShapeFactory.java
@@ -1,2 +1,11 @@
-public abstract class ShapeFactory {
+package model;
+
+public final class ShapeFactory {
+    public Shape createProduct(Position position, float rotation, Position rotationCenter, Position translation, String color, float width, float height, float borderRadius) {
+        return new Rectangle(position, rotation, rotationCenter, translation, color, width, height, borderRadius);
+    }
+
+    public Shape createProduct(Position position, float rotation, Position rotationCenter, Position translation, String color, int edges, float length) {
+        return new Polygon(position, rotation, rotationCenter, translation, color, edges, length);
+    }
 }
